7969f0c8ac99a32ead32a83fa568b69bc7145113
FTWinston <ftwinston@gmail.com>
2020-04-08 11:09:17 +0100

    Removed Newtonsoft.Json references

--- RobotInterrogation/Models/InterviewStatus.cs
+++ RobotInterrogation/Models/InterviewStatus.cs
@@ -1,9 +1,5 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-
-namespace RobotInterrogation.Models
+﻿namespace RobotInterrogation.Models
 {
-    [JsonConverter(typeof(StringEnumConverter))]
     public enum InterviewStatus
     {
         WaitingForConnections,
